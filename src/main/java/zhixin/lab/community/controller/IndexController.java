324de3bebe65d650df6d3413a64b661bffbcf1a4
Annielina <1037004391@qq.com>
2020-07-21 15:31:15 +0800

    aaddbootstrap

--- src/main/java/zhixin/lab/community/controller/IndexController.java
+++ src/main/java/zhixin/lab/community/controller/IndexController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
-public class HelloController {    //处理器handler
+public class IndexController {    //处理器handler
     //业务方法
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(){
+
+        return "index";
 
     }
 }
